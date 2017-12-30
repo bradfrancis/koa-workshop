f0b23971eb5655007c463b89b75e3cd6f06a0c20
Brad Francis <brad.francis.90@gmail.com>
2017-12-30 11:42:30 +1100

    Exercise 6 completed! (I think..)

--- 06-content-negotiation/index.js
+++ 06-content-negotiation/index.js
@@ -1,4 +1,3 @@
-
 var koa = require('koa');
 
 /**
@@ -10,10 +9,32 @@ var koa = require('koa');
  *     this.response.body = yield gzip('something');
  *   })
  */
-var gzip = require('mz/zlib').gzip;
+//var gzip = require('mz/zlib').gzip;
+var zlib = require('zlib');
 
 var app = module.exports = koa();
 
-app.use(function* () {
+gzip = function(buf) {
+    return new Promise((resolve, reject) => {
+        zlib.gzip(buf, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
 
-})
+app.use(function* (next) {
+    if (this.request.acceptsEncodings('gzip')) {
+        this.response.set('Content-Encoding', 'gzip');
+        this.response.body = yield gzip('hello world');
+    } 
+    else if (this.request.acceptsEncodings('identity')){
+        this.response.set('Content-Encoding', 'identity');
+        this.response.body = 'hello world';
+    }
+    else {
+        yield* next;
+    }
+});
+
+//app.listen(3000);
