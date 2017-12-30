c28c8d7bf11a07f0df041b4e798c6974edaeb77f
Brad Francis <brad.francis.90@gmail.com>
2017-12-30 11:53:33 +1100

    Exercise 7 completed!

--- 07-content-headers/index.js
+++ 07-content-headers/index.js
@@ -4,5 +4,7 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* () {
-  
-})
+    this.response.body = (this.request.is('application/json'))
+        ? {message: 'hi!'}
+        : 'ok';
+});
