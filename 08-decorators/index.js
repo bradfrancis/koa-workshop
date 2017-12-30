395197a14517d11ed1817ac4d8f949ca8e195ef9
Brad Francis <brad.francis.90@gmail.com>
2017-12-30 12:01:01 +1100

    Exercise 8 completed!

--- 08-decorators/index.js
+++ 08-decorators/index.js
@@ -6,9 +6,12 @@ var app = module.exports = koa();
 
 app.use(function* (next) {
   yield next;
-  // add some logic here!
+  this.response.body = escape(this.response.body);
 })
 
 app.use(function* body() {
   this.response.body = 'this following HTML should be escaped: <p>hi!</p>';
 });
+
+
+//app.listen(3000);
